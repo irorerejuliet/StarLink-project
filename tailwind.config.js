25684b930874bdc42e25e8e862301bd522d3f637
irorere juliet <irorerejuliet@gmail.com>
2025-01-21 14:55:04 -0900

    work on the main section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     extend: {
       backgroundImage: {
         homeImg: "url('/src/image/home-hero.jpg')",
+        creativityimg: "url('/src/image/creativityimg.jpeg')",
+        earthImg: "url('/src/image/earth-img.jpeg')"
+      },
+      fontFamily: {
+        Roboto: ["Roboto", "serif"],
+        Arimo: ["Arimo", "serif"],
       },
     },
   },
