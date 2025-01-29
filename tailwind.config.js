2bb9ec5290db5d1a43a4690980c4221a09d4c68a
irorere juliet <irorerejuliet@gmail.com>
2025-01-28 22:57:26 -0900

    make correction on the bg inage

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,15 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        homeImg: "url('./src/image/home-hero.jpg')",
-        creativityimg: "url('./src/image/creativityimg.jpeg')",
-        earthImg: "url('./src/image/earth-img.jpeg')",
-        rockectimg: "url('./src/image/rockect-img.jpeg')",
-        Residential: "url('./src/image/residential_bg.jpeg')",
-        Residential2: "url('./src/image/residential_main-bg.jpeg')",
-        highSpeed: "url('./src/image/highSpeed-bg.jpeg')",
-        weather: "url('./src/image/weatherResilent-img.jpeg')",
-        hardware: "url('./src/image/hardware-bg.jpeg')",
+        homeImg: "url('/src/image/home-hero.jpg')",
+        creativityimg: "url('/src/image/creativityimg.jpeg')",
+        earthImg: "url('/src/image/earth-img.jpeg')",
+        rockectimg: "url('/src/image/rockect-img.jpeg')",
+        Residential: "url('/src/image/residential_bg.jpeg')",
+        Residential2: "url('/src/image/residential_main-bg.jpeg')",
+        highSpeed: "url('/src/image/highSpeed-bg.jpeg')",
+        weather: "url('/src/image/weatherResilent-img.jpeg')",
+        hardware: "url('/src/image/hardware-bg.jpeg')",
       },
       fontFamily: {
         Roboto: ["Roboto", "serif"],
